67cd377b30eefc3016ad2aabbed1c5fa2bc852cf
Rodrigo-Co <141284747+Rodrigo-Co@users.noreply.github.com>
2024-10-29 22:48:59 -0300

    vamov

--- app.js
+++ app.js
@@ -26,10 +26,10 @@ app.use(bodyParser.json());
 
 // Configuração do banco de dados
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', // Substitua pelo seu usuário do MySQL
-    password: 'rodrigo', // Substitua pela sua senha do MySQL
-    database: 'bancotb', // Nome do seu banco de dados
+    host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 db.connect((err) => { 
